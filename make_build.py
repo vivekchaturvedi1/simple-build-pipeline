9eb813123569971be42cd73170288906e80647f5
Vivek Chaturvedi <vivek.chaturvedi@vertisinfotech.com>
2014-12-10 18:29:34 +0530

    Made some changes to follow the standard python coding conventions

--- make_build.py
+++ make_build.py
@@ -1,20 +1,30 @@
-#stdlibRoutine
+#stdlib
+import json
+
+#stdlib routines
 from os import environ
 from subprocess import check_output
 from uuid import uuid4
+
+#3rd party lib routines
 from fabric.api import cd, run, settings, env
 
-env.host_string='vivek@10.10.10.60:22'
-env.password='vivek!'
+#with context manager
+with open("config/env_data.json", 'r') as fp:
+  env_data=json.load(fp)
+
+#setting env varibles
+env.host_string=env_data['host_string']
+env.password=env_data['password']
 
 def main():
   repoPath='https://github.com/vivekchaturvedi1/cmake-hello-world.git'
   repoName=repoPath.rpartition('/')[2].rpartition('.')[0]
-  repoDirName=str(uuid4().get_hex().upper()[0:6])
+  workingDirectory=str(uuid4().get_hex().upper()[0:6])
   repoDirPath=environ["HOME"]
-  repoDirAbsPath=repoDirPath + '/' + repoDirName
+  repoDirAbsPath=repoDirPath + '/' + workingDirectory
 
-  runShell('mkdir %s' %repoDirName, '%s' %repoDirPath)
+  runShell('mkdir %s' %workingDirectory, '%s' %repoDirPath)
   runShell('git clone %s' %repoPath, '%s' %repoDirAbsPath)
   runShell('mkdir build', '%s/%s' %(repoDirAbsPath, repoName))
   runShell('cmake ..', '%s/%s/build' %(repoDirAbsPath, repoName))
@@ -22,12 +32,15 @@ def main():
   runShell('./CMakeHelloWorld', '%s/%s/build' %(repoDirAbsPath, repoName))
 
 
-def runShell(cmdArg, cwdArg):
-
+def runShell(command, directory):
+  """
+    :param command: 'shell command'
+    :param directory: 'the directory-path where to run the shell command'
+  """
   with settings(warn_only=True):
-    with cd(cwdArg):
-      if run(cmdArg).failed:
-        run("echo %s Failed!" %cmdArg)
+    with cd(directory):
+      if run(command).failed:
+        run("echo %s Failed!" %command)
 
 if __name__ == "__main__":
   main()
